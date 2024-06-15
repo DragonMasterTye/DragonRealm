d7796e64c1b490faec65f12177e02ff405241968
Landon Morrison <DragonmasterTye@gmail.com>
2024-06-15 15:40:22 -0700

    Checkpoint 1
    - Basic Character Movement Input
    - Updated gitignore for externals

---
+++ DragonRealm/Source/DragonRealm/Private/DR_Character.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DragonRealm/Public/DR_Character.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+ADR_Character::ADR_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	SpringArmComponent->SetupAttachment(RootComponent);
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+// Called when the game starts or when spawned
+void ADR_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ADR_Character::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void ADR_Character::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+/*void ADR_Character::Turn(float Value)
+{
+	
+}
+
+void ADR_Character::LookUp(float Value)
+{
+}*/
+
+// Called every frame
+void ADR_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADR_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ADR_Character::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ADR_Character::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);	
+}
+
---
+++ DragonRealm/Source/DragonRealm/Public/DR_Character.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "DR_Character.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+UCLASS()
+class DRAGONREALM_API ADR_Character : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ADR_Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComponent;
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComponent;
+
+	// Movement Functions
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	/*void Turn(float Value);
+	void LookUp(float Value);*/
+	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
